afae1f43d83ef02c995ccb9a59966e3e672cf674
paulabeatrizz <paulinhabeatrizcontato@gmail.com>
2024-09-06 19:39:21 -0300

    inicializando o springboot

---
+++ src/main/java/br/com/upe/ECHOTEXT/EchotextApplication.java
@@ -0,0 +1,13 @@
+package br.com.upe.ECHOTEXT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchotextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchotextApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/upe/ECHOTEXT/EchotextApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.upe.ECHOTEXT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchotextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
