1c4a1241bb9f8277abae24878121496c1e1144f5
Sophia <eloiselamenha@gmail.com>
2024-09-24 22:38:47 -0300

    Tesseract semi funcional
    Pra ficar completamente funcional ainda precisa adaptar pra ele ler imagem input de usuário
    PASSO A PASSO PRA FUNCIONAR:
    - instalar tesseract (incluir pt, copiar path)
    - ⁠pip install pytesseract
    - pip install opencv-python
    - pip install python-dotenv
    - cria um arquivo .env e cria uma variável CAMINHO e cola nela o caminho para o tesseract.exe na sua máquina

--- views.py
+++ views.py
@@ -1,8 +1,20 @@
 from app import app
 from flask import render_template
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 @app.route("/")
 def menuinicial():
+    import pytesseract
+    import cv2
+
+    imagem = cv2.imread("testee.png")
+
+    pytesseract.pytesseract.tesseract_cmd = os.getenv("CAMINHO")
+    texto = pytesseract.image_to_string(imagem, lang="por")
+
     return render_template("menuinicial.html")
 
 @app.route("/sobrenos")
