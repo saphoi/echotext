27e572081ede41726434abf37023bcc4e55977be
Sophia <eloiselamenha@gmail.com>
2024-10-07 14:15:25 -0300

    BACK FUNCIONANDO

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 import os
 from dotenv import load_dotenv
 from gtts import gTTS
@@ -7,26 +7,29 @@ import pygame
 
 load_dotenv()
 
-@app.route("/", endpoint='index')
+@app.route("/", endpoint='index', methods=['GET', 'POST'])
 def index():
     import pytesseract
     import cv2
+    if request.method == 'POST':
+        file = request.form['file']
+        imagem = cv2.imread(file)
 
-    imagem = cv2.imread("testee.png")
+        pytesseract.pytesseract.tesseract_cmd = os.getenv("CAMINHO")
+        texto = pytesseract.image_to_string(imagem, lang="por")
 
-    pytesseract.pytesseract.tesseract_cmd = os.getenv("CAMINHO")
-    texto = pytesseract.image_to_string(imagem, lang="por")
+        tts = gTTS(text=texto, lang='pt')
+        audio_file = "audio.mp3"  
+        tts.save(audio_file)  
 
-    tts = gTTS(text=texto, lang='pt')
-    audio_file = "audio.mp3"  
-    tts.save(audio_file)  
+        pygame.mixer.init()
+        pygame.mixer.music.load(audio_file)
+        pygame.mixer.music.play()
+        #os.system("start audio.mp3")
 
-    pygame.mixer.init()
-    pygame.mixer.music.load(audio_file)
-    pygame.mixer.music.play()
-    #os.system("start audio.mp3")
-
-    return render_template("index.html")
+        return render_template("index.html")
+    else:
+        return render_template("index.html")
 
 @app.route("/about")
 def about():
